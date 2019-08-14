742c98211ef69e95e1ad8379103f4502deae020d
mythkiven <mythziven@gmail.com>
2019-08-14 21:09:19 +0800

    1、报错：'string' file not found   ；修改方案：C++标准库修改为libc++ 2、crash: fwrite写入空值crash  ；修改方案：判空即可 3、部分 KVC 增加判空处理； 4、进度条更新代码放入主线程中； Version:2.4.9130

--- Document.mm
+++ Document.mm
@@ -448,28 +448,30 @@ enum ViewType
 //----------------------------------------------------------------------------
 - (void)handleThreadStateChanged:(NSNotification *)notification
 {
-  if ([notification object] == dataController)
-  {
-    NSString * threadState = [[notification userInfo] objectForKey:MVStatusUserInfoKey];
-    if ([threadState isEqualToString:MVStatusTaskStarted] == YES)
-    {
-      if (OSAtomicIncrement32(&threadCount) == 1)
+    dispatch_async(dispatch_get_main_queue(), ^{
+      if ([notification object] == dataController)
       {
-        [progressIndicator setUsesThreadedAnimation:YES];
-        [progressIndicator startAnimation:nil];
-        [stopButton setHidden:NO];
+        NSString * threadState = [[notification userInfo] objectForKey:MVStatusUserInfoKey];
+        if ([threadState isEqualToString:MVStatusTaskStarted] == YES)
+        {
+          if (OSAtomicIncrement32(&threadCount) == 1)
+          {
+            [progressIndicator setUsesThreadedAnimation:YES];
+            [progressIndicator startAnimation:nil];
+            [stopButton setHidden:NO];
+          }
+        }
+        else if ([threadState isEqualToString:MVStatusTaskTerminated] == YES)
+        {
+          if (OSAtomicDecrement32(&threadCount) == 0)
+          {
+            [progressIndicator stopAnimation:nil];
+            [statusText setStringValue:@""];
+            [stopButton setHidden:YES];
+          }
+        }
       }
-    }
-    else if ([threadState isEqualToString:MVStatusTaskTerminated] == YES)
-    {
-      if (OSAtomicDecrement32(&threadCount) == 0)
-      {
-        [progressIndicator stopAnimation:nil]; 
-        [statusText setStringValue:@""];
-        [stopButton setHidden:YES];
-      }
-    }
-  }
+    });
 }
 
 //----------------------------------------------------------------------------
